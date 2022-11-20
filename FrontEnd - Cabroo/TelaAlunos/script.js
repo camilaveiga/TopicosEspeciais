bad795f0b1480411ba88fd56fa33ccd6d91e2a81
Camila <cb.veiga3@gmail.com>
2022-11-20 16:37:55 -0300

    Atualizacao das telas

---
+++ FrontEnd - Cabroo/TelaAlunos/script.js
@@ -0,0 +1,11 @@
+function TelaSobre(){
+    window.location.href="../TelaSobre/index.html"
+}
+
+function TelaCadastroPais(){
+    window.location.href="../TelaCadastroPais/index.html"
+}
+
+function TelaCadastroAluno(){
+    window.location.href="../TelaCadastroAluno/index.html"
+}
---
+++ FrontEnd - Cabroo/TelaMenu/script.js
@@ -0,0 +1,15 @@
+function TelaSobre(){
+    window.location.href="../TelaSobre/index.html"
+}
+
+function TelaCadastroPais(){
+    window.location.href="../TelaCadastroPais/index.html"
+}
+
+function TelaCadastroAluno(){
+    window.location.href="../TelaCadastroAluno/index.html"
+}
+
+function TelaAlunos(){
+    window.location.href="../TelaAlunos/index.html"
+}
